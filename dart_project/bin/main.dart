738c3ce6018e66850fd3f52f898560cd8a830556
Justin Ram <justramin000@gmail.com>
2025-03-20 17:02:44 +0530

    first commit

---
+++ dart_project/bin/main.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+// void main() {
+//   // print('Hello, World1!');
+//   stdout.write('Enter the first value : ');
+//   var inputA = stdin.readLineSync();
+//   stdout.write('Enter the Second value : ');
+//   var inputB = stdin.readLineSync();
+
+//   var num1 = int.parse(inputA!);
+//   var num2 = int.parse(inputB!);
+//   var sum = num1 + num2;
+//   print('Total is : $sum');
+// }
+
+void main() {}
