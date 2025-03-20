5b45a7b9aec5b7a2e8aa93404951196300992259
Justin Ram <justramin000@gmail.com>
2025-03-20 17:29:18 +0530

    data types

--- dart_project/bin/main.dart
+++ dart_project/bin/main.dart
@@ -1,16 +1,35 @@
 import 'dart:io';
 
+// void main() {
+//   // print('Hello, World1!');
+//   stdout.write('Enter the first value : ');
+//   var inputA = stdin.readLineSync();
+//   stdout.write('Enter the Second value : ');
+//   var inputB = stdin.readLineSync();
+
+//   var num1 = int.parse(inputA!);
+//   var num2 = int.parse(inputB!);
+//   var sum = num1 + num2;
+//   print('Total is : $sum');
+// }
+
+//Data Type
+
+int a = 10;
+double b = 20.0;
+num num1 = 10;
+num num2 = 20.0;
+String name = 'justin';
+String name2 = " hi i'am Justin ";
+String address = ''' punathil house 
+po kariyad south ''';
 void main() {
-  // print('Hello, World1!');
-  stdout.write('Enter the first value : ');
-  var inputA = stdin.readLineSync();
-  stdout.write('Enter the Second value : ');
-  var inputB = stdin.readLineSync();
-
-  var num1 = int.parse(inputA!);
-  var num2 = int.parse(inputB!);
-  var sum = num1 + num2;
-  print('Total is : $sum');
+  print(a);
+  print(b);
+  print(num1);
+  print(num2);
+  print(name);
+  print(name2);
+  print(address);
+  print(address.length);
 }
-
-// void main() {}
